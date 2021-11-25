28770d11548b2aaec843dc1262511199fca93de9
Jarod79 <89936533+Jarod79@users.noreply.github.com>
2021-11-25 14:28:37 +0100

    Added fonts modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 import "./App.css";
-import Cards from "./components/Cards";
+import CardList from "./components/CardsList";
 import Modal from "./components/Modal";
-=======
-import './App.css';
-import Cards from './components/CardsList'
->>>>>>> 2bd07b25943389404f4a070051bb0fdde3d59561
 
 function App() {
   return (
     <div className="App">
       <Modal />
-      <Cards />
+      <CardList />
     </div>
   );
 }
--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./cardslist.css";
 import Card from "./Card";
 
 const CardsList = () => {
-    const [playlist, setPlaylist] = useState();
-    const [track, setTrack] = useState([]);
+  const [playlist, setPlaylist] = useState();
+  const [track, setTrack] = useState([]);
 
-    useEffect(() => {
-        axios
-            .get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/5135745864`)
-            .then((response) => response.data.tracks.data)
-            .then((data) => 
-            setPlaylist(data)
-            )
-          .catch((error) => console.error(error))
-    }, []);
+  useEffect(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/5135745864`
+      )
+      .then((response) => response.data.tracks.data)
+      .then((data) => setPlaylist(data))
+      .catch((error) => console.error(error));
+  }, []);
 
-    console.log(playlist)
-    return(
-        <div>
-            {playlist &&
-            playlist.map((track, index) => 
-                <Card cover={track.album.cover} title={track.title} track={track.track} key={index} />
-            )
-            }
-                
-        </div>
-    )
-}
+  console.log(playlist);
+  return (
+    <div>
+      {playlist &&
+        playlist.map((track, index) => (
+          <Card
+            cover={track.album.cover}
+            title={track.title}
+            track={track.track}
+            key={index}
+          />
+        ))}
+    </div>
+  );
+};
 
 export default CardsList;
