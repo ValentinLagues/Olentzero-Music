2456b5e3c1eec69375d82ce6c0cacfa144048e7d
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-11-25 22:51:51 +0100

    lottie

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+// IMAGES
 import image1 from "./images/1.png";
 import image2 from "./images/2.png";
 import image3 from "./images/3.png";
@@ -23,158 +24,188 @@ import image22 from "./images/22.png";
 import image23 from "./images/23.png";
 import image24 from "./images/24.png";
 import image25 from "./images/25.png";
+// SONGS
 import song1 from "./mp3/Wham-LastChristmas.mp3";
 import song2 from "./mp3/OlentzeroTxirriMirrietaTxiribiton.mp3";
+
+// LOTTIE
+import lottie1 from "./lotties/1.json";
+import lottie2 from "./lotties/2.json";
+import lottie3 from "./lotties/3.json";
+import lottie4 from "./lotties/4.json";
+import lottie5 from "./lotties/5.json";
+import lottie6 from "./lotties/6.json";
+import lottie7 from "./lotties/7.json";
+import lottie8 from "./lotties/8.json";
+import lottie9 from "./lotties/9.json";
+import lottie10 from "./lotties/10.json";
+import lottie11 from "./lotties/11.json";
+import lottie12 from "./lotties/12.json";
+import lottie13 from "./lotties/13.json";
+import lottie14 from "./lotties/14.json";
+import lottie15 from "./lotties/15.json";
+import lottie16 from "./lotties/16.json";
+import lottie17 from "./lotties/17.json";
+import lottie18 from "./lotties/18.json";
+import lottie19 from "./lotties/19.json";
+import lottie20 from "./lotties/20.json";
+import lottie21 from "./lotties/21.json";
+import lottie22 from "./lotties/22.json";
+import lottie23 from "./lotties/23.json";
+import lottie24 from "./lotties/24.json";
+import lottie25 from "./lotties/25.json";
+
+
 
 const calendar = [
   {
     number: "1",
     image: image1,
-    lottie: "",
+    lottie: lottie1,
     url: song1,
   },
   {
     number: "2",
     image: image2,
-    lottie: "",
+    lottie: lottie2,
     url: song2,
   },
   {
     number: "3",
     image: image3,
-    lottie: "",
+    lottie: lottie3,
     url: "https://www.youtube.com/watch?v=j5ceLMPlqnA",
   },
   {
     number: "4",
     image: image4,
-    lottie: "",
+    lottie: lottie4,
     url: "https://www.youtube.com/watch?v=tjpvdX1qaKY",
   },
   {
     number: "5",
     image: image5,
-    lottie: "",
+    lottie: lottie5,
     url: "https://www.youtube.com/watch?v=16fZV9uOvyA",
   },
   {
     number: "6",
     image: image6,
-    lottie: "",
+    lottie: lottie6,
     url: "https://www.youtube.com/watch?v=bLvdG5LMwr0&list=PLF57367E2FD3E4203",
   },
   {
     number: "7",
     image: image7,
-    lottie: "",
+    lottie: lottie7,
     url: "https://www.youtube.com/watch?v=EXa0zHdD4mE",
   },
   {
     number: "8",
     image: image8,
-    lottie: "",
+    lottie: lottie8,
     url: "https://www.youtube.com/watch?v=5bIpVZcMvjw",
   },
   {
     number: "9",
     image: image9,
-    lottie: "",
+    lottie: lottie9,
     url: "https://www.youtube.com/watch?v=cc_f5MmP7k0&list=PLF57367E2FD3E4203&index=3",
   },
   {
     number: "10",
     image: image10,
-    lottie: "",
+    lottie: lottie10,
     url: "https://www.youtube.com/watch?v=A_vA2-yJmr4",
   },
   {
     number: "11",
     image: image11,
-    lottie: "",
+    lottie: lottie11,
     url: "https://www.youtube.com/watch?v=LsCd7biuDng&list=PLCbyUblvTyKm3-SkG23u6TW-u2XzzZxw_&index=18",
   },
   {
     number: "12",
     image: image12,
-    lottie: "",
+    lottie: lottie12,
     url: "https://www.youtube.com/watch?v=pC2sngTuB9k",
   },
   {
     number: "13",
     image: image13,
-    lottie: "",
+    lottie: lottie13,
     url: "https://www.youtube.com/watch?v=gr2MLUfuWlQ",
   },
   {
     number: "14",
     image: image14,
-    lottie: "",
+    lottie: lottie14,
     url: "https://www.youtube.com/watch?v=v6FSAJ1Hw4c",
   },
   {
     number: "15",
     image: image15,
-    lottie: "",
+    lottie: lottie15,
     url: "https://www.youtube.com/watch?v=hzZmsGvx18k",
   },
   {
     number: "16",
     image: image16,
-    lottie: "",
+    lottie: lottie16,
     url: "https://www.youtube.com/watch?v=IA-zXBiH6JQ&list=PLF57367E2FD3E4203&index=4",
   },
   {
     number: "17",
     image: image17,
-    lottie: "",
+    lottie: lottie17,
     url: "https://www.youtube.com/watch?v=1DPtUa_tlUo",
   },
   {
     number: "18",
     image: image18,
-    lottie: "",
+    lottie: lottie18,
     url: "https://www.youtube.com/watch?v=70cYeWeDG5g&list=PLF57367E2FD3E4203&index=6",
   },
   {
     number: "19",
     image: image19,
-    lottie: "",
+    lottie: lottie19,
     url: "https://www.youtube.com/watch?v=yHto3CoDICs&list=PLCbyUblvTyKm3-SkG23u6TW-u2XzzZxw_&index=20",
   },
   {
     number: "20",
     image: image20,
-    lottie: "",
+    lottie: lottie20,
     url: "https://www.youtube.com/watch?v=C9QHtOnEIHk",
   },
   {
     number: "21",
     image: image21,
-    lottie: "",
+    lottie: lottie21,
     url: "https://www.youtube.com/watch?v=1ffl301Ba9o",
   },
   {
     number: "22",
     image: image22,
-    lottie: "",
+    lottie: lottie22,
     url: "https://www.youtube.com/watch?v=5Vwu-t7QRaE",
   },
   {
     number: "23",
     image: image23,
-    lottie: "",
+    lottie: lottie23,
     url: "https://www.youtube.com/watch?v=vbvyNnw8Qjg",
   },
   {
     number: "24",
     image: image24,
-    lottie: "",
+    lottie: lottie24,
     url: "https://www.youtube.com/watch?v=kFhf7pjRRjA",
   },
   {
     number: "25",
     image: image25,
-    lottie: "",
+    lottie: lottie25,
     url: "https://www.youtube.com/watch?v=aAkMkVFwAoo",
   },
 ];
