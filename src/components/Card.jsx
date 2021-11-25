3bc22122c51c8f6b4275a2be0d50bcc77b2678d3
mickacharp <mickaelcharp@gmail.com>
2021-11-25 21:42:26 +0100

    grid correctly displayed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import "./card.css";
+import "./cardslist.css";
 
 const Card = ({ track, number, image }) => {
-    const [playing, setPlaying] = useState(false);
+  const [playing, setPlaying] = useState(false);
 
-    console.log(number);
-    return (
-        <div className="container" onClick={() => setPlaying(!playing)}>
-            <img src={image} alt="{}" />
-            <h1>{number}</h1>
-            <ReactPlayer className="player" url={track} controls playing={playing} volume={0.1} />
-        </div>
-    );
+  console.log(number);
+  return (
+    <div
+      className={`day-${number} container`}
+      onClick={() => setPlaying(!playing)}
+    >
+      <img src={image} alt="{}" />
+      <h1>{number}</h1>
+      {/* <ReactPlayer
+        className="player"
+        url={track}
+        controls
+        playing={playing}
+        volume={0.1}
+      /> */}
+    </div>
+  );
 };
 
 export default Card;
--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./cardslist.css";
 import Card from "./Card";
 import calendar from "../data";
 
 const CardsList = () => {
-
-    return (
-        <div className="cardsGrid">
-        {calendar.map((day) => (
-            <Card number={day.number} image={day.image} track={day.url} />
-        ))}
-        </div>
-    );
+  return (
+    <div className="grid-avent">
+      {calendar.map((day) => (
+        <Card number={day.number} image={day.image} /* track={day.url} */ />
+      ))}
+    </div>
+  );
 };
 
 export default CardsList;
