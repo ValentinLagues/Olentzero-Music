b663876368241f5b426a486bd7f12f79984f89e6
Jarod79 <89936533+Jarod79@users.noreply.github.com>
2021-11-25 17:44:17 +0100

    Crad switch to click

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import "./card.css";
 
 const Card = ({ title, track, cover, number, image }) => {
+  const [cardReturn, setCardreturn] = useState(true);
   console.log(number);
   return (
-    <div className="container">
-      <img src={image} alt="{}" />
-      <h1>{number}</h1>
-      <ReactPlayer url={track} controls playing volume={0.1} />
-    </div>
+    <>
+      <div className="container" onClick={() => setCardreturn(!cardReturn)}>
+        {cardReturn && (
+          <div>
+            <img src={image} alt="{}" />
+            <h1>{number}</h1>
+          </div>
+        )}
+
+        {!cardReturn && (
+          <div>
+            <ReactPlayer url={track} controls playing volume={0.1} />
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./cardslist.css";
 import Card from "./Card";
 import calendar from "../data";
