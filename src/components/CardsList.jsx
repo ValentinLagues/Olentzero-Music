560a9adcf67d89d9ade22c93b019d52996ba1f4b
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-11-25 22:23:57 +0100

    track played resolved

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./cardslist.css";
 import Card from "./Card";
 import calendar from "../data";
 
 const CardsList = () => {
-  console.log(calendar[0].url);
-  return (
-    <div className="cardsGrid">
-      {calendar.map((day, index) => (
-        <Card
-          key={index}
-          number={day.number}
-          image={day.image}
-          track={day.url}
-        />
-      ))}
-    </div>
-  );
+    const [playing, setPlaying] = useState(0);
+
+    console.log(calendar[0].url);
+    return (
+        <div className="cardsGrid">
+        {calendar.map((day, index) => (
+            <Card
+            key={index}
+            number={day.number}
+            image={day.image}
+            track={day.url}
+            setPlaying={setPlaying}
+            playing={playing}
+            />
+        ))}
+        </div>
+    );
 };
 
 export default CardsList;
