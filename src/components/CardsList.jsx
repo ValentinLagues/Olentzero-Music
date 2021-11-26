37cdf7f839b60cd9ff1140a1092cb70e8c4758ca
mickacharp <mickaelcharp@gmail.com>
2021-11-26 08:03:31 +0100

    responsive calendar grid & flex OK

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -3,26 +3,24 @@ import "./cardslist.css";
 import Card from "./Card";
 import calendar from "../data";
 
-const CardsList = ({darkMode}) => {
-    const [activeCard, setActiveCard] = useState(0);
+const CardsList = ({ darkMode }) => {
+  const [activeCard, setActiveCard] = useState(0);
 
-
-
-    return (
-      <div className={darkMode ? "grid-avent-dark" : "grid-avent"}>
-        {calendar.map((day, index) => (
-          <Card
-            key={index}
-            number={day.number}
-            image={day.image}
-            track={day.url}
-            lottie={day.lottie}
-            setActiveCard={setActiveCard}
-            activeCard={activeCard}
-          />
-        ))}
-      </div>
-    );
+  return (
+    <div className={darkMode ? "grid-advent-dark" : "grid-advent"}>
+      {calendar.map((day, index) => (
+        <Card
+          key={index}
+          number={day.number}
+          image={day.image}
+          track={day.url}
+          lottie={day.lottie}
+          setActiveCard={setActiveCard}
+          activeCard={activeCard}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default CardsList;
