5f9c4fb7360b100152bfccd48ea6be3f0c10d881
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-11-26 09:03:25 +0100

    a

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -3,18 +3,20 @@ import "./darkmode.css";
 const DarkMode = ({ setDarkMode, darkMode }) => {
   return (
     <div className={darkMode ? "dark-mode" : "mode"}>
-      <label
+      {/* <label
         className={darkMode ? "dark-mode-label" : "mode-label"}
         htmlFor="checkbox"
       >
         Dark Mode
-      </label>
+      </label> */}
       <button
-        className="dark-mode-checkbox"
+        className={darkMode ? "dark-mode-label" : "mode-label"}
         type="button"
         value={darkMode}
         onClick={() => setDarkMode(!darkMode)}
-      />
+      >
+        {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
     </div>
   );
 };
