943e07ca2ad6034043c959d1d68a6905edb3086f
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-11-26 07:00:23 +0100

    a

---
+++ src/dark-data.js
@@ -0,0 +1,236 @@
+// IMAGES
+import image1 from "./images/1.png";
+import image2 from "./images/2.png";
+import image3 from "./images/3.png";
+import image4 from "./images/4.png";
+import image5 from "./images/5.png";
+import image6 from "./images/6.png";
+import image7 from "./images/7.png";
+import image8 from "./images/8.png";
+import image9 from "./images/9.png";
+import image10 from "./images/10.png";
+import image11 from "./images/11.png";
+import image12 from "./images/12.png";
+import image13 from "./images/13.png";
+import image14 from "./images/14.png";
+import image15 from "./images/15.png";
+import image16 from "./images/16.png";
+import image17 from "./images/17.png";
+import image18 from "./images/18.png";
+import image19 from "./images/19.png";
+import image20 from "./images/20.png";
+import image21 from "./images/21.png";
+import image22 from "./images/22.png";
+import image23 from "./images/23.png";
+import image24 from "./images/24.png";
+import image25 from "./images/25.png";
+
+// SONGS
+import song1 from "./mp3-dark/AHS.mp3";
+import song2 from "./mp3-dark/Alien.mp3";
+import song3 from "./mp3-dark/Exorcist.mp3";
+import song4 from "./mp3-dark/Chucky.mp3";
+import song5 from "./mp3-dark/Saw.mp3";
+import song6 from "./mp3-dark/Freddy.mp3";
+import song7 from "./mp3-dark/Ghostbusters.mp3";
+import song8 from "./mp3-dark/Halloween.mp3";
+import song9 from "./mp3-dark/Psycho.mp3";
+import song10 from "./mp3-dark/Hellraiser.mp3";
+import song11 from "./mp3-dark/HillsHaveEyes.mp3";
+import song12 from "./mp3-dark/TheThing.mp3";
+import song13 from "./mp3-dark/Jaws.mp3";
+import song14 from "./mp3-dark/Leatherface.mp3";
+import song15 from "./mp3-dark/Nosferatu.mp3";
+import song16 from "./mp3-dark/Prometheus.mp3";
+import song17 from "./mp3-dark/Halloween3.mp3";
+import song18 from "./mp3-dark/Beetlejuice.mp3";
+import song19 from "./mp3-dark/SilentHill.mp3";
+import song20 from "./mp3-dark/StrangerThings.mp3";
+import song21 from "./mp3-dark/TheOmen.mp3";
+import song22 from "./mp3-dark/Jason.mp3";
+import song23 from "./mp3-dark/TwilightZone.mp3";
+import song24 from "./mp3-dark/XFiles.mp3";
+import song25 from "./mp3-dark/MrJack.mp3";
+
+// LOTTIE
+import lottie1 from "./lotties-dark/AHS.gif";
+import lottie2 from "./lotties-dark/Alien.gif";
+import lottie3 from "./lotties-dark/Beetlejuice.gif";
+import lottie4 from "./lotties-dark/Chucky.gif";
+import lottie5 from "./lotties-dark/Exorcist.gif";
+import lottie6 from "./lotties-dark/Freddy.gif";
+import lottie7 from "./lotties-dark/Ghostbusters.gif";
+import lottie8 from "./lotties-dark/Halloween.gif";
+import lottie9 from "./lotties-dark/Halloween3.gif";
+import lottie10 from "./lotties-dark/Hellraiser.gif";
+import lottie11 from "./lotties-dark/HillsHaveEyes.gif";
+import lottie12 from "./lotties-dark/Jason.gif";
+import lottie13 from "./lotties-dark/Jaws.gif";
+import lottie14 from "./lotties-dark/Leatherface.gif";
+import lottie15 from "./lotties-dark/Nosferatu.gif";
+import lottie16 from "./lotties-dark/Prometheus.gif";
+import lottie17 from "./lotties-dark/Psycho.gif";
+import lottie18 from "./lotties-dark/Saw.gif";
+import lottie19 from "./lotties-dark/SilentHill.gif";
+import lottie20 from "./lotties-dark/StrangerThings.gif";
+import lottie21 from "./lotties-dark/TheOmen.gif";
+import lottie22 from "./lotties-dark/TheThing.gif";
+import lottie23 from "./lotties-dark/TwilightZone.gif";
+import lottie24 from "./lotties-dark/XFiles.gif";
+import lottie25 from "./lotties-dark/MrJack.gif";
+
+
+const darkCalendar = [
+  {
+    number: "1",
+    image: image1,
+    lottie: lottie1,
+    url: song1,
+  },
+  {
+    number: "2",
+    image: image2,
+    lottie: lottie2,
+    url: song2,
+  },
+  {
+    number: "3",
+    image: image3,
+    lottie: lottie3,
+    url: song3,
+  },
+  {
+    number: "4",
+    image: image4,
+    lottie: lottie4,
+    url: song4,
+  },
+  {
+    number: "5",
+    image: image5,
+    lottie: lottie5,
+    url: song5,
+  },
+  {
+    number: "6",
+    image: image6,
+    lottie: lottie6,
+    url: song6,
+  },
+  {
+    number: "7",
+    image: image7,
+    lottie: lottie7,
+    url: song7,
+  },
+  {
+    number: "8",
+    image: image8,
+    lottie: lottie8,
+    url: song8,
+  },
+  {
+    number: "9",
+    image: image9,
+    lottie: lottie9,
+    url: song9,
+  },
+  {
+    number: "10",
+    image: image10,
+    lottie: lottie10,
+    url: song10,
+  },
+  {
+    number: "11",
+    image: image11,
+    lottie: lottie11,
+    url: song11,
+  },
+  {
+    number: "12",
+    image: image12,
+    lottie: lottie12,
+    url: song12,
+  },
+  {
+    number: "13",
+    image: image13,
+    lottie: lottie13,
+    url: song13,
+  },
+  {
+    number: "14",
+    image: image14,
+    lottie: lottie14,
+    url: song14,
+  },
+  {
+    number: "15",
+    image: image15,
+    lottie: lottie15,
+    url: song15,
+  },
+  {
+    number: "16",
+    image: image16,
+    lottie: lottie16,
+    url: song16,
+  },
+  {
+    number: "17",
+    image: image17,
+    lottie: lottie17,
+    url: song17,
+  },
+  {
+    number: "18",
+    image: image18,
+    lottie: lottie18,
+    url: song18,
+  },
+  {
+    number: "19",
+    image: image19,
+    lottie: lottie19,
+    url: song19,
+  },
+  {
+    number: "20",
+    image: image20,
+    lottie: lottie20,
+    url: song20,
+  },
+  {
+    number: "21",
+    image: image21,
+    lottie: lottie21,
+    url: song21,
+  },
+  {
+    number: "22",
+    image: image22,
+    lottie: lottie22,
+    url: song22,
+  },
+  {
+    number: "23",
+    image: image23,
+    lottie: lottie23,
+    url: song23,
+  },
+  {
+    number: "24",
+    image: image24,
+    lottie: lottie24,
+    url: song24,
+  },
+  {
+    number: "25",
+    image: image25,
+    lottie: lottie25,
+    url: song25,
+  },
+];
+
+export default darkCalendar;
